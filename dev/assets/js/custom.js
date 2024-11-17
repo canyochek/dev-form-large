005b674e39e51ad1a79374ae7c02c23513da04fd
canyochek <aleks47898@gmail.com>
2024-11-17 13:31:44 +0200

    lesson(20-23)

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,1 +1,16 @@
-  
+$(document).ready(function() {
+  $('select').niceSelect();
+  $('.phone').mask('+00-000-000-00-00');
+  $('[data-index]').mask('00000');
+});
+
+
+
+  $("[date-calendar]").wrap("<div class='calendar-container'></div>");
+
+  $( "[date-calendar]" ).datepicker({
+    showOn: "button",
+    buttonImage: "assets/img/calendar-icon.svg",
+    buttonImageOnly: true,
+    buttonText: "Select date"
+   });
